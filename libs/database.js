548376fc21222061d2a2281a7f71ab06454de7ec
unknown <tobidaramola77@gmail.com>
2023-11-16 22:19:32 +0100

    bugss

--- libs/database.js
+++ libs/database.js
@@ -6,15 +6,13 @@ const {
   MONGO_PORT,
 } = require("../config/config");
 
-const connectWithRetry = async () => {
-  const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
-  mongoose
-    .connect(mongoURL)
-    .then(() => console.log("Connected!"))
-    .catch((err) => {
-      console.log(err);
-      setTimeout(connectWithRetry, 5000);
-    });
-};
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose
+  .connect(mongoURL)
+  .then(() => console.log("Connected!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 module.exports = connectWithRetry;

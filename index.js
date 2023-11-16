f1a09af345c9beb0394ad0628daec331ffd40efe
unknown <tobidaramola77@gmail.com>
2023-11-16 08:57:36 +0100

    bugs bugs bugs

--- index.js
+++ index.js
@@ -2,17 +2,35 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { REDIS_URL, SESSION_SECRET, REDIS_PORT } = require("./config/config");
-const connectWithRetry = require("./libs/database");
+const mongoose = require("mongoose");
+//const connectWithRetry = require("./libs/database");
 
 const session = require("express-session");
 const redis = require("redis");
 const RedisStore = require("connect-redis").default;
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const userRouter = require("./routes/userRoutes");
 const postRouter = require("./routes/postRoutes");
 
 const app = express();
 app.use(cors());
+
+const connectWithRetry = async () => {
+  const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("Connected!"))
+    .catch((err) => {
+      console.log(err);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
 
 connectWithRetry();
 

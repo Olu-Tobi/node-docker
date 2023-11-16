a1131c3d6d1660358c7debafd44a3c40a120f44e
unknown <tobidaramola77@gmail.com>
2023-11-16 22:02:33 +0100

    bugs

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const postRouter = require("./routes/postRoutes");
 const app = express();
 app.use(cors());
 
-//connectWithRetry();
-
 const client = redis.createClient({
   url: `${REDIS_URL}://${REDIS_URL}:${REDIS_PORT}`,
 });
--- libs/database.js
+++ libs/database.js
@@ -8,13 +8,13 @@ const {
 
 const connectWithRetry = async () => {
   const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
-  // mongoose
-  //   .connect(mongoURL)
-  //   .then(() => console.log("Connected!"))
-  //   .catch((err) => {
-  //     console.log(err);
-  //     setTimeout(connectWithRetry, 5000);
-  //   });
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("Connected!"))
+    .catch((err) => {
+      console.log(err);
+      setTimeout(connectWithRetry, 5000);
+    });
 };
 
 module.exports = connectWithRetry;

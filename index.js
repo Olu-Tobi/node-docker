e093fac60e73d00fc5185c89dd237effe9756995
unknown <tobidaramola77@gmail.com>
2023-11-20 09:32:47 +0100

    ingtegrated docker swarm

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(
 app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
-  res.send("<h2>Hi  There</h2>");
+  res.send("<h2>Ok let's have some fun with docker!</h2>");
   console.log("yeah it ran");
 });
 
